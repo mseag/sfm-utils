33e563699654f298c4da3b0f4ee8beff414494c3
laineyhm <laineyhm@gmail.com>
2022-12-20 11:21:32 +0700

    create sfm.ts; recursively list txt files from directory

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as books from './books';
 import * as toolbox from './toolbox';
+import * as sfm from './sfm';
 const {version} = require('../package.json');
 
 ////////////////////////////////////////////////////////////////////
@@ -79,9 +80,26 @@ if (options.text) {
   // Parse a txt file into JSON Object
   filesToParse.push(options.text);
 } else if (options.directory) {
-// TODO: Handle options.directory to get list of all txt files to process and ignore certain directories
-
+  // Get a list of all txt files in options.directory and ignore certain directories
+  getTextFilesInside(options.directory, filesToParse);
 }
+
+// Recursively puts text files from a directory into an array
+function getTextFilesInside(directory, fileArray){
+  fs.readdirSync(directory).forEach(function(file){
+    const absolutePath = path.join(directory, file);
+    if (fs.statSync(absolutePath).isDirectory()){
+      return getTextFilesInside(absolutePath, fileArray);
+    }
+    else{
+      if(file.substring(file.lastIndexOf('.')+1, file.length) === 'txt'){
+        fileArray.push(absolutePath);
+      }
+      return;
+    }
+  });
+}
+
 
 if (options.json) {
   // Get the book status from the JSON file
@@ -106,53 +124,5 @@ if (options.json) {
 
 // Write out the JSON Objects to SFM
 if (bookObj) {
-
-  const ID_MARKER = "\\id ";
-  const USFM_MARKER = "\\usfm ";
-  const HEADER_MARKER = "\\h ";
-  const TOC_MARKER = "\\toc ";
-  const MAIN_TITLE_MARKER = "\\mt ";
-  const CHAPTER_MARKER = "\\c ";
-  const SECTION_MARKER = "\\s1 ";
-  const PARAGRAPH_MARKER = "\n\\p";
-  const VERSE_MARKER = "\\v ";
-  const CRLF = "\n";
-
-  const chapters = bookObj.content;
-
-  let SFMtext = "";
-
-  SFMtext += ID_MARKER + bookObj.header.bookInfo.code + ' ' +  bookObj.header.projectName + CRLF;
-  SFMtext += USFM_MARKER + '3.0' + CRLF;
-  SFMtext += HEADER_MARKER + bookObj.header.bookInfo.name + CRLF;
-  SFMtext += TOC_MARKER + bookObj.header.bookInfo.name + CRLF;
-  SFMtext += MAIN_TITLE_MARKER + bookObj.header.bookInfo.name + CRLF;
-
-
-  chapters.forEach(function(chapter) {
-    if(chapter.number != 0){
-      SFMtext += CHAPTER_MARKER + chapter.number + CRLF;
-      if(chapter.content){
-        const sectionsAndVerses = chapter.content;
-        sectionsAndVerses.forEach(function(unit) {
-          switch(unit.type) {
-            case "section":
-              SFMtext += SECTION_MARKER + unit.text + PARAGRAPH_MARKER + CRLF;
-              break;
-            case "verse":
-              SFMtext += VERSE_MARKER + unit.number + ' ' + unit.text + CRLF;
-              break;
-            default:
-              throw 'Invalid type on ' + JSON.stringify(unit) + '. \nLooking for "section" or "verse".';
-          }
-        });
-      }
-    }
-  });
-
-  const bookNum = bookObj.header.bookInfo.num;
-  const bookCode = bookObj.header.bookInfo.code;
-  const projectName = bookObj.header.projectName;
-  fs.writeFileSync('./' + bookNum + bookCode + projectName + '.SFM', SFMtext);
-
+  sfm.convertToSFM(bookObj);
 }
---
+++ src/sfm.ts
@@ -0,0 +1,58 @@
+import * as books from './books';
+import * as fs from 'fs';
+
+/**
+ * Parse a JSON file and converts it to USFM
+ * @param {Books.objType} bookObj - a book type of JSON object
+ */
+export function convertToSFM(bookObj: books.objType) : any {
+
+  const ID_MARKER = "\\id ";
+  const USFM_MARKER = "\\usfm ";
+  const HEADER_MARKER = "\\h ";
+  const TOC_MARKER = "\\toc ";
+  const MAIN_TITLE_MARKER = "\\mt ";
+  const CHAPTER_MARKER = "\\c ";
+  const SECTION_MARKER = "\\s1 ";
+  const PARAGRAPH_MARKER = "\n\\p";
+  const VERSE_MARKER = "\\v ";
+  const CRLF = "\n";
+
+  const chapters = bookObj.content;
+
+  let SFMtext = "";
+
+  SFMtext += ID_MARKER + bookObj.header.bookInfo.code + ' ' +  bookObj.header.projectName + CRLF;
+  SFMtext += USFM_MARKER + '3.0' + CRLF;
+  SFMtext += HEADER_MARKER + bookObj.header.bookInfo.name + CRLF;
+  SFMtext += TOC_MARKER + bookObj.header.bookInfo.name + CRLF;
+  SFMtext += MAIN_TITLE_MARKER + bookObj.header.bookInfo.name + CRLF;
+
+
+  chapters.forEach(function(chapter) {
+    if(chapter.number != 0){
+      SFMtext += CHAPTER_MARKER + chapter.number + CRLF;
+      if(chapter.content){
+        const sectionsAndVerses = chapter.content;
+        sectionsAndVerses.forEach(function(unit) {
+          switch(unit.type) {
+            case "section":
+              SFMtext += SECTION_MARKER + unit.text + PARAGRAPH_MARKER + CRLF;
+              break;
+            case "verse":
+              SFMtext += VERSE_MARKER + unit.number + ' ' + unit.text + CRLF;
+              break;
+            default:
+              throw 'Invalid type on ' + JSON.stringify(unit) + '. \nLooking for "section" or "verse".';
+          }
+        });
+      }
+    }
+  });
+
+  const bookNum = bookObj.header.bookInfo.num;
+  const bookCode = bookObj.header.bookInfo.code;
+  const projectName = bookObj.header.projectName;
+  fs.writeFileSync('./' + bookNum + bookCode + projectName + '.SFM', SFMtext);
+
+}
