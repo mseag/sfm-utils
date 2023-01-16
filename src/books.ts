5d706428950327a68943d64ebf8a9180ba2ed2aa
Darcy Wong <darcy_wong@sil.org>
2023-01-16 15:43:26 +0700

    fix: Add books.versesInChapter and books.unitSubtype (#3)
    * fix: Add books.versesInChapter and books.unitSubtype
    
    Part of changes needed to handle additional set of SFM files
    
    * Add versesInChapter as a way to flag if more verses are unexpectedly processed for a particular book
    * Add unitSubtype
    
    * fix: More versesInChapter corrections
    
    * fix: More corrections on versesInChapter
    
    * Update src/books.ts

--- src/books.ts
+++ src/books.ts
@@ -32,6 +32,7 @@ export interface bookType {
   name: string;     // Book name
   num: number;      // Book number
   chapters: number; // Total number of chapters in the book
+  versesInChapter: number[]; // Number of verses in each chapter (placeholder at 0)
   verses: number;   // Total number of verses in the book
 }
 
@@ -46,6 +47,7 @@ export const bookInfo: bookType[] = [
     name: "Placeholder",
     num: 0,
     chapters: 0,
+    versesInChapter: [0],
     verses: 0
   },
 
@@ -55,6 +57,11 @@ export const bookInfo: bookType[] = [
     name: "Genesis",
     num: 1,
     chapters: 50,
+    versesInChapter: [0, 31, 25, 24, 26, 32, 22, 24, 22, 29, 32,
+                         32, 20, 18, 24, 21, 16, 27, 33, 38, 18,
+                         34, 24, 20, 67, 34, 35, 46, 22, 35, 43,
+                         55, 32, 20, 31, 29, 43, 36, 30, 23, 23,
+                         57, 38, 34, 34, 28, 34, 31, 22, 33, 26],
     verses: 1533
   },
   {
@@ -62,6 +69,10 @@ export const bookInfo: bookType[] = [
     name: "Exodus",
     num: 2,
     chapters: 40,
+    versesInChapter: [0, 22, 25, 22, 31, 23, 30, 25, 32, 35, 29,
+                         10, 51, 22, 31, 27, 36, 16, 27, 25, 26,
+                         36, 31, 33, 18, 40, 37, 21, 43, 46, 38,
+                         18, 35, 23, 35, 35, 38, 29, 31, 43, 38],
     verses: 1213
   },
   {
@@ -69,6 +80,9 @@ export const bookInfo: bookType[] = [
     name: "Leviticus",
     num: 3,
     chapters: 27,
+    versesInChapter: [0, 17, 16, 17, 35, 19, 30, 38, 36, 24, 20,
+                         47,  8, 59, 57, 33, 34, 16, 30, 37, 27,
+                         24, 33, 44, 23, 55, 46, 34],
     verses: 859
   },
   {
@@ -76,6 +90,10 @@ export const bookInfo: bookType[] = [
     name: "Numbers",
     num: 4,
     chapters: 36,
+    versesInChapter: [0, 54, 34, 51, 49, 31, 27, 89, 26, 23, 36,
+                         35, 16, 33, 45, 41, 50, 13, 32, 22, 29,
+                         35, 41, 30, 25, 18, 65, 23, 31, 40, 16,
+                         54, 42, 56, 29, 34, 13],
     verses: 1288
   },
   {
@@ -83,6 +101,10 @@ export const bookInfo: bookType[] = [
     name: "Deuteronomy",
     num: 5,
     chapters: 34,
+    versesInChapter: [0, 46, 37, 29, 49, 33, 25, 26, 20, 29, 22,
+                         32, 32, 18, 29, 23, 22, 20, 22, 21, 20,
+                         23, 30, 25, 22, 19, 19, 26, 68, 29, 20,
+                         30, 52, 29, 12],
     verses: 959
   },
   {
@@ -90,6 +112,9 @@ export const bookInfo: bookType[] = [
     name: "Joshua",
     num: 6,
     chapters: 24,
+    versesInChapter: [0, 18, 24, 17, 24, 15, 27, 26, 35, 27, 43,
+                         23, 24, 33, 15, 63, 10, 18, 28, 51,  9,
+                         45, 34, 16, 33],
     verses: 658
   },
   {
@@ -97,6 +122,9 @@ export const bookInfo: bookType[] = [
     name: "Judges",
     num: 7,
     chapters: 21,
+    versesInChapter: [0, 36, 23, 31, 24, 31, 40, 25, 35, 57, 18,
+                         40, 15, 25, 20, 20, 31, 13, 31, 30, 48,
+                         25],
     verses: 618
   },
   {
@@ -104,6 +132,7 @@ export const bookInfo: bookType[] = [
     name: "Ruth",
     num: 8,
     chapters: 4,
+    versesInChapter: [0, 22, 23, 18, 22],
     verses: 85
   },
   {
@@ -111,6 +140,10 @@ export const bookInfo: bookType[] = [
     name: "1 Samuel",
     num: 9,
     chapters: 31,
+    versesInChapter: [0, 28, 36, 21, 22, 12, 21, 17, 22, 27, 27,
+                         15, 25, 23, 52, 35, 23, 58, 30, 24, 42,
+                         15, 23, 29, 22, 44, 25, 12, 25, 11, 31,
+                         13],
     verses: 810
   },
   {
@@ -118,6 +151,9 @@ export const bookInfo: bookType[] = [
     name: "2 Samuel",
     num: 10,
     chapters: 24,
+    versesInChapter: [0, 27, 32, 39, 12, 25, 23, 29, 18, 13, 19,
+                         27, 31, 39, 33, 37, 23, 29, 33, 43, 26,
+                         22, 51, 39, 25],
     verses: 695
   },
   {
@@ -125,6 +161,9 @@ export const bookInfo: bookType[] = [
     name: "1 Kings",
     num: 11,
     chapters: 22,
+    versesInChapter: [0, 53, 46, 28, 34, 18, 38, 51, 66, 28, 29,
+                         43, 33, 34, 31, 34, 34, 24, 46, 21, 43,
+                         29, 53],
     verses: 816
   },
   {
@@ -132,6 +171,9 @@ export const bookInfo: bookType[] = [
     name: "2 Kings",
     num: 12,
     chapters: 25,
+    versesInChapter: [0, 18, 25, 27, 44, 27, 33, 20, 29, 37, 36,
+                         21, 21, 25, 29, 38, 20, 41, 37, 37, 21,
+                         26, 20, 37, 20, 30],
     verses: 719
   },
   {
@@ -139,6 +181,9 @@ export const bookInfo: bookType[] = [
     name: "1 Chronicles",
     num: 13,
     chapters: 29,
+    versesInChapter: [0, 54, 55, 24, 43, 26, 81, 40, 40, 44, 14,
+                         47, 40, 14, 17, 29, 43, 27, 17, 19,  8,
+                         30, 19, 32, 31, 31, 32, 34, 21, 30],
     verses: 942
   },
   {
@@ -146,6 +191,10 @@ export const bookInfo: bookType[] = [
     name: "2 Chronicles",
     num: 14,
     chapters: 36,
+    versesInChapter: [0, 17, 18, 17, 22, 14, 42, 22, 18, 31, 19,
+                         23, 16, 22, 15, 19, 14, 19, 34, 11, 37,
+                         20, 12, 21, 27, 28, 23,  9, 27, 36, 27,
+                         21, 33, 25, 33, 27, 23],
     verses: 822
   },
   {
@@ -153,6 +202,7 @@ export const bookInfo: bookType[] = [
     name: "Ezra",
     num: 15,
     chapters: 10,
+    versesInChapter: [0, 11, 70, 13, 24, 17, 22, 28, 36, 15, 44],
     verses: 280
   },
   {
@@ -160,6 +210,8 @@ export const bookInfo: bookType[] = [
     name: "Nehemiah",
     num: 16,
     chapters: 13,
+    versesInChapter: [0, 11, 20, 32, 23, 19, 19, 73, 18, 38, 39,
+                         36, 47, 31],
     verses: 406
   },
   {
@@ -167,6 +219,7 @@ export const bookInfo: bookType[] = [
     name: "Esther",
     num: 17,
     chapters: 10,
+    versesInChapter: [0, 22, 23, 15, 17, 14, 14, 10, 17, 32, 3],
     verses: 167
   },
   {
@@ -174,13 +227,37 @@ export const bookInfo: bookType[] = [
     name: "Job",
     num: 18,
     chapters: 42,
-    verses: 150
+    versesInChapter: [0, 22, 13, 26, 21, 27, 30, 21, 22, 35, 22,
+                         20, 25, 28, 22, 35, 22, 16, 21, 29, 29,
+                         34, 30, 17, 25,  6, 14, 23, 28, 25, 31,
+                         40, 22, 33, 37, 16, 33, 24, 41, 30, 24,
+                         34, 17],
+    verses: 1070
   },
   {
     code: "PSA",
     name: "Psalms",
     num: 19,
     chapters: 150,
+    versesInChapter: [0,  6, 12,  8,  8, 12, 10, 17,  9, 20, 18,
+                          7,  8,  6,  7,  5, 11, 15, 50, 14,  9,
+                         13, 31,  6, 10, 22, 12, 14,  9, 11, 12,
+                         24, 11, 22, 22, 28, 12, 40, 22, 13, 17,
+                         13, 11,  5, 26, 17, 11,  9, 14, 20, 23,
+
+                         // 51-100
+                         19,  9,  6,  7, 23, 13, 11, 11, 17, 12,
+                          8, 12, 11, 10, 13, 20,  7, 35, 36,  5,
+                         24, 20, 28, 23, 10, 12, 20, 72, 13, 19,
+                         16,  8, 18, 12, 13, 17,  7, 18, 52, 17,
+                         16, 15,  5, 23, 11, 13, 12,  9,  9,  5,
+
+                         // 101-150
+                          8, 28, 22, 35, 45, 48, 43, 13, 31,  7,
+                         10, 10,  9,  8, 18, 19,  2, 29, 176, 7,
+                          8,  9,  4 , 8,  5,  6,  5,  6,  8,  8,
+                          3, 18,  3,  3, 21, 26,  9,  8, 24, 13,
+                         10,  7, 12, 15, 21, 10, 20, 14, 9, 6],
     verses: 2461
   },
   {
@@ -188,6 +265,10 @@ export const bookInfo: bookType[] = [
     name: "Proverbs",
     num: 20,
     chapters: 31,
+    versesInChapter: [0, 33, 22, 35, 27, 23, 35, 27, 36, 18, 32,
+                         31, 28, 25, 35, 33, 33, 28, 24, 29, 30,
+                         31, 29, 35, 34, 28, 28, 27, 28, 27, 33,
+                         31],
     verses: 915
   },
   {
@@ -195,6 +276,8 @@ export const bookInfo: bookType[] = [
     name: "Ecclesiastes",
     num: 21,
     chapters: 12,
+    versesInChapter: [0, 18, 26, 22, 16, 20, 12, 29, 17, 18, 20,
+                         10, 14],
     verses: 222
   },
   {
@@ -202,6 +285,7 @@ export const bookInfo: bookType[] = [
     name: "Song of Songs",
     num: 22,
     chapters: 8,
+    versesInChapter: [0, 17, 17, 11, 16, 16, 13, 13, 14],
     verses: 117
   },
   {
@@ -209,6 +293,13 @@ export const bookInfo: bookType[] = [
     name: "Isaiah",
     num: 23,
     chapters: 66,
+    versesInChapter: [0, 31, 22, 26,  6, 30, 13, 25, 22, 21, 34,
+                         16,  6, 22, 32,  9, 14, 14,  7, 25,  6,
+                         17, 25, 18, 23, 12, 21, 13, 29, 24, 33,
+                          9, 20, 24, 17, 10, 22, 38, 22,  8, 31,
+                         29, 25, 28, 28, 25, 13, 15, 22, 26, 11,
+                         23, 15, 12, 17, 13, 12, 21, 14, 21, 22,
+                         11, 12, 19, 12, 25, 24],
     verses: 1292
   },
   {
@@ -216,6 +307,12 @@ export const bookInfo: bookType[] = [
     name: "Jeremiah",
     num: 24,
     chapters: 52,
+    versesInChapter: [0, 19, 37, 25, 31, 31, 30, 34, 22, 26, 25,
+                         23, 17, 27, 22, 21, 21, 27, 23, 15, 18,
+                         14, 30, 40, 10, 38, 24, 22, 17, 32, 24,
+                         40, 44, 26, 22, 19, 32, 21, 28, 18, 16,
+                         18, 22, 13, 30,  5, 28,  7, 47, 39, 46,
+                         64, 34],
     verses: 1364
   },
   {
@@ -223,6 +320,7 @@ export const bookInfo: bookType[] = [
     name: "Lamentations",
     num: 25,
     chapters: 5,
+    versesInChapter: [0, 22, 22, 66, 22, 22, ],
     verses: 154
   },
   {
@@ -230,6 +328,11 @@ export const bookInfo: bookType[] = [
     name: "Ezekiel",
     num: 26,
     chapters: 48,
+    versesInChapter: [0, 28, 10, 27, 17, 17, 14, 27, 18, 11, 22,
+                         25, 28, 23, 23,  8, 63, 24, 32, 14, 49,
+                         32, 31, 49, 27, 17, 21, 36, 26, 21, 26,
+                         18, 32, 33, 31, 15, 38, 28, 23, 29, 49,
+                         26, 20, 27, 31, 25, 24, 23, 35],
     verses: 1273
   },
   {
@@ -237,6 +340,8 @@ export const bookInfo: bookType[] = [
     name: "Daniel",
     num: 27,
     chapters: 12,
+    versesInChapter: [0, 21, 49, 30, 37, 31, 28, 28, 27, 27, 21,
+                         45, 13],
     verses: 357
   },
   {
@@ -244,6 +349,8 @@ export const bookInfo: bookType[] = [
     name: "Hosea",
     num: 28,
     chapters: 14,
+    versesInChapter: [0, 11, 23,  5, 19, 15, 11, 16, 14, 17, 15,
+                         12, 14, 16,  9],
     verses: 197
   },
   {
@@ -251,6 +358,7 @@ export const bookInfo: bookType[] = [
     name: "Joel",
     num: 29,
     chapters: 3,
+    versesInChapter: [0, 20, 32, 21],
     verses: 73
   },
   {
@@ -258,6 +366,7 @@ export const bookInfo: bookType[] = [
     name: "Amos",
     num: 30,
     chapters: 9,
+    versesInChapter: [0, 15, 16, 15, 13, 27, 14, 17, 14, 15],
     verses: 146
   },
   {
@@ -265,6 +374,7 @@ export const bookInfo: bookType[] = [
     name: "Obadiah",
     num: 31,
     chapters: 1,
+    versesInChapter: [0, 21],
     verses: 21
   },
   {
@@ -272,6 +382,7 @@ export const bookInfo: bookType[] = [
     name: "Jonah",
     num: 32,
     chapters: 4,
+    versesInChapter: [0, 17, 10, 10, 11],
     verses: 48
   },
   {
@@ -279,6 +390,7 @@ export const bookInfo: bookType[] = [
     name: "Micah",
     num: 33,
     chapters: 7,
+    versesInChapter: [0, 16, 13, 12, 13, 15, 16, 20],
     verses: 105
   },
   {
@@ -286,6 +398,7 @@ export const bookInfo: bookType[] = [
     name: "Nahum",
     num: 34,
     chapters: 3,
+    versesInChapter: [0, 15, 13, 19],
     verses: 47
   },
   {
@@ -293,6 +406,7 @@ export const bookInfo: bookType[] = [
     name: "Habakkuk",
     num: 35,
     chapters: 3,
+    versesInChapter: [0, 17, 20, 19],
     verses: 56
   },
   {
@@ -300,6 +414,7 @@ export const bookInfo: bookType[] = [
     name: "Zephaniah",
     num: 36,
     chapters: 3,
+    versesInChapter: [0, 18, 15, 20],
     verses: 53
   },
   {
@@ -307,6 +422,7 @@ export const bookInfo: bookType[] = [
     name: "Haggai",
     num: 37,
     chapters: 2,
+    versesInChapter: [0, 15, 23],
     verses: 38
   },
   {
@@ -314,6 +430,8 @@ export const bookInfo: bookType[] = [
     name: "Zechariah",
     num: 38,
     chapters: 14,
+    versesInChapter: [0, 21, 13, 10, 14, 11, 15, 14, 23, 17, 12,
+                         17, 14,  9, 21],
     verses: 211
   },
   {
@@ -321,6 +439,7 @@ export const bookInfo: bookType[] = [
     name: "Malachi",
     num: 39,
     chapters: 4,
+    versesInChapter: [0, 14, 17, 18, 6],
     verses: 55
   },
 
@@ -330,6 +449,9 @@ export const bookInfo: bookType[] = [
     name: "Matthew",
     num: 40,
     chapters: 28,
+    versesInChapter: [0, 25, 23, 17, 25, 48, 34, 29, 34, 38, 42,
+                         30, 50, 58, 36, 39, 28, 27, 35, 30, 34,
+                         46, 46, 39, 51, 46, 75, 66, 20],
     verses: 1071
   },
   {
@@ -337,6 +459,8 @@ export const bookInfo: bookType[] = [
     name: "Mark",
     num: 41,
     chapters: 16,
+    versesInChapter: [0, 45, 28, 35, 41, 43, 56, 37, 38, 50, 52,
+                         33, 44, 37, 72, 47, 20],
     verses: 678
   },
   {
@@ -344,6 +468,9 @@ export const bookInfo: bookType[] = [
     name: "Luke",
     num: 42,
     chapters: 24,
+    versesInChapter: [0, 80, 52, 38, 44, 39, 49, 50, 56, 62, 42,
+                         54, 59, 35, 35, 32, 31, 37, 43, 48, 47,
+                         38, 71, 56, 53],
     verses: 1151
   },
   {
@@ -351,6 +478,9 @@ export const bookInfo: bookType[] = [
     name: "John",
     num: 43,
     chapters: 21,
+    versesInChapter: [0, 51, 25, 36, 54, 47, 71, 53, 59, 41, 42,
+                         57, 50, 38, 31, 27, 33, 26, 40, 42, 31,
+                         25],
     verses: 879
   },
   {
@@ -358,6 +488,9 @@ export const bookInfo: bookType[] = [
     name: "Acts",
     num: 44,
     chapters: 28,
+    versesInChapter: [0, 26, 47, 26, 37, 42, 15, 60, 40, 43, 48,
+                         30, 25, 52, 28, 41, 40, 34, 28, 41, 38,
+                         40, 30, 35, 27, 27, 32, 44, 31],
     verses: 1007
   },
   {
@@ -365,6 +498,8 @@ export const bookInfo: bookType[] = [
     name: "Romans",
     num: 45,
     chapters: 16,
+    versesInChapter: [0, 32, 29, 31, 25, 21, 23, 25, 39, 33, 21,
+                         36, 21, 14, 23, 33, 27],
     verses: 433
   },
   {
@@ -372,6 +507,8 @@ export const bookInfo: bookType[] = [
     name: "1 Corinthians",
     num: 46,
     chapters: 16,
+    versesInChapter: [0, 31, 16, 23, 21, 13, 20, 40, 13, 27, 33,
+                         34, 31, 13, 40, 58, 24],
     verses: 437
   },
   {
@@ -379,6 +516,8 @@ export const bookInfo: bookType[] = [
     name: "2 Corinthians",
     num: 47,
     chapters: 13,
+    versesInChapter: [0, 24, 17, 18, 18, 21, 18, 16, 24, 15, 18,
+                         33, 21, 14],
     verses: 257
   },
   {
@@ -386,6 +525,7 @@ export const bookInfo: bookType[] = [
     name: "Galatians",
     num: 48,
     chapters: 6,
+    versesInChapter: [0, 24, 21, 29, 31, 26, 18],
     verses: 149
   },
   {
@@ -393,6 +533,7 @@ export const bookInfo: bookType[] = [
     name: "Ephesians",
     num: 49,
     chapters: 6,
+    versesInChapter: [0, 23, 22, 21, 32, 33, 24],
     verses: 155
   },
   {
@@ -400,6 +541,7 @@ export const bookInfo: bookType[] = [
     name: "Philippians",
     num: 50,
     chapters: 4,
+    versesInChapter: [0, 30, 30, 21, 23],
     verses: 104
   },
   {
@@ -407,6 +549,7 @@ export const bookInfo: bookType[] = [
     name: "Colossians",
     num: 51,
     chapters: 4,
+    versesInChapter: [0, 29, 23, 25, 18],
     verses: 95
   },
   {
@@ -414,6 +557,7 @@ export const bookInfo: bookType[] = [
     name: "1 Thessalonians",
     num: 52,
     chapters: 5,
+    versesInChapter: [0, 10, 20, 13, 18, 28],
     verses: 89
   },
   {
@@ -421,6 +565,7 @@ export const bookInfo: bookType[] = [
     name: "2 Thessalonians",
     num: 53,
     chapters: 3,
+    versesInChapter: [0, 12, 17, 18],
     verses: 47
   },
   {
@@ -428,6 +573,7 @@ export const bookInfo: bookType[] = [
     name: "1 Timothy",
     num: 54,
     chapters: 6,
+    versesInChapter: [0, 20, 15, 16, 16, 25, 21],
     verses: 113
   },
   {
@@ -435,6 +581,7 @@ export const bookInfo: bookType[] = [
     name: "2 Timothy",
     num: 55,
     chapters: 4,
+    versesInChapter: [0, 18, 26, 17, 22],
     verses: 83
   },
   {
@@ -442,6 +589,7 @@ export const bookInfo: bookType[] = [
     name: "Titus",
     num: 56,
     chapters: 3,
+    versesInChapter: [0, 16, 15, 15],
     verses: 46
   },
   {
@@ -449,6 +597,7 @@ export const bookInfo: bookType[] = [
     name: "Philemon",
     num: 57,
     chapters: 1,
+    versesInChapter: [0, 25],
     verses: 25
   },
   {
@@ -456,6 +605,8 @@ export const bookInfo: bookType[] = [
     name: "Hebrews",
     num: 58,
     chapters: 13,
+    versesInChapter: [0, 14, 18, 19, 16, 14, 20, 28, 13, 28, 39,
+                         40, 29, 25],
     verses: 303
   },
   {
@@ -463,6 +614,7 @@ export const bookInfo: bookType[] = [
     name: "James",
     num: 59,
     chapters: 5,
+    versesInChapter: [0, 27, 26, 18, 17, 20],
     verses: 108
   },
   {
@@ -470,6 +622,7 @@ export const bookInfo: bookType[] = [
     name: "1 Peter",
     num: 60,
     chapters: 5,
+    versesInChapter: [0, 25, 25, 22, 19, 14],
     verses: 105
   },
   {
@@ -477,6 +630,7 @@ export const bookInfo: bookType[] = [
     name: "2 Peter",
     num: 61,
     chapters: 3,
+    versesInChapter: [0, 21, 22, 18],
     verses: 61
   },
   {
@@ -484,6 +638,7 @@ export const bookInfo: bookType[] = [
     name: "1 John",
     num: 62,
     chapters: 5,
+    versesInChapter: [0, 10, 29, 24, 21, 21],
     verses: 105
   },
   {
@@ -491,6 +646,7 @@ export const bookInfo: bookType[] = [
     name: "2 John",
     num: 63,
     chapters: 1,
+    versesInChapter: [0, 13],
     verses: 13
   },
   {
@@ -498,6 +654,7 @@ export const bookInfo: bookType[] = [
     name: "3 John",
     num: 64,
     chapters: 1,
+    versesInChapter: [0, 14],
     verses: 14
   },
   {
@@ -505,6 +662,7 @@ export const bookInfo: bookType[] = [
     name: "Jude",
     num: 65,
     chapters: 1,
+    versesInChapter: [0, 25],
     verses: 25
   },
   {
@@ -512,14 +670,26 @@ export const bookInfo: bookType[] = [
     name: "Revelation",
     num: 66,
     chapters: 22,
+    versesInChapter: [0, 20, 29, 22, 11, 14, 17, 17, 13, 21, 11,
+                         19, 17, 18, 20,  8, 21, 18, 24, 21, 15,
+                         27, 21],
     verses: 404
   }
 ];
 //#endregion
 
+/**
+ * Description of the unit within a chapter.
+ */
+export type unitSubtype =
+  "padding" |
+  "chapter" |
+  "verse" |
+  "section";
+
 export interface unitType {
-  type: string,
-  number: number,
+  type: unitSubtype,
+  number: number, // Verse number or section title level
   content?: any,
   text?: string
 }
@@ -553,6 +723,23 @@ export function getBookByCode(bookCode: CodeType): bookType {
     process.exit(1);
   }
   return book;
+}
+
+/**
+ * Test utility to count the number of verses in each chapter
+ */
+export function countVerses() {
+  bookInfo.forEach(b => {
+    if (b.versesInChapter) {
+      let totalVerses = 0;
+      b.versesInChapter.forEach(v => {
+        totalVerses += v;
+      })
+      if (totalVerses != b.verses) {
+        console.error(`${b.name} counted ${totalVerses} but should be ${b.verses}`);
+      }
+    }
+  });
 }
 
 /**
@@ -631,6 +818,12 @@ export function getBookByName(name: string): bookType {
     case '1John':
       bookName = '1 John';
       break;
+    case '2John':
+      bookName = '2 John';
+      break;
+    case '3John':
+        bookName = '3 John';
+        break;
     default:
       bookName = name;
   }
